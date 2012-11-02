acc9cb647c7c01fd510909bcfbe9024af3bc3fcd
Alex Bourgeois <alex@GreenGoblin.(none)>
2012-11-02 09:58:47 -0400

    Changed file structure :)

--- link.sh
+++ link.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd $HOME
 rm $HOME/.profile.backup
-mv $HOME/.profile $HOME/.profile.backup
-ln -s $HOME/config/.profile .profile
-ln -s $HOME/config/.vimrc .vimrc
-ln -s $HOME/config/.gitconfig .gitconfig
+mv $HOME/.profile $HOME/backup/.profile.backup
+ln -s $HOME/git/config/.profile .profile
+ln -s $HOME/git/config/.vimrc .vimrc
