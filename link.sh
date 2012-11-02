ecb52650b80cb048f045613885cb0ae1581b85a0
Alex BOURGEOIS <abourg28@gmail.com>
2012-11-01 21:04:37 -0400

    Changed prompt and some other things

--- link.sh
+++ link.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd $HOME
+rm $HOME/.profile.backup
+mv $HOME/.profile $HOME/.profile.backup
 ln -s $HOME/config/.profile .profile
 ln -s $HOME/config/.vimrc .vimrc
 ln -s $HOME/config/.gitconfig .gitconfig
